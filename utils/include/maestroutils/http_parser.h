8da54450bba8f956109a6692f562808fc56c2ca8
tjackr <tjackr@proton.me>
2026-02-09 22:42:20 +0100

    update include in http_parser

--- utils/include/maestroutils/http_parser.h
+++ utils/include/maestroutils/http_parser.h
@@ -8,7 +8,7 @@
 #include <maestromodules/linked_list.h>
 #include <maestroutils/HTTPStatusCodes.h>
 #include <maestroutils/error.h>
-#include <maestroutils/misc_utils.h>
+#include <maestroutils/string_utils.h>
 
 #include <ctype.h>
 #include <errno.h>
